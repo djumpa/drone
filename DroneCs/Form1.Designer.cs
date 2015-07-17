836695e1601b1fd7f53bdadf34bcbd8df5d1f0a1
djumpa <FildaRak@seznam.cz>
2015-07-17 10:43:13 +0200

    C# project for visual studio

---
+++ DroneCs/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace DroneCs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_GPS = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.refresh = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.sendTimer = new System.Windows.Forms.Timer(this.components);
+            this.label_Acc = new System.Windows.Forms.Label();
+            this.label_telemetry = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ping Result";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "10.10.1.59";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Remote Address";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ping";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_GPS
+            // 
+            this.label_GPS.AutoSize = true;
+            this.label_GPS.Location = new System.Drawing.Point(218, 85);
+            this.label_GPS.Name = "label_GPS";
+            this.label_GPS.Size = new System.Drawing.Size(55, 13);
+            this.label_GPS.TabIndex = 5;
+            this.label_GPS.Text = "GPS Data";
+            this.label_GPS.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(44, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "8888";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Enabled = true;
+            this.refresh.Tick += new System.EventHandler(this.refresh_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(482, 68);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 231);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 10;
+            this.trackBar2.Location = new System.Drawing.Point(533, 68);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 231);
+            this.trackBar2.SmallChange = 5;
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 10;
+            this.trackBar3.Location = new System.Drawing.Point(584, 68);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 231);
+            this.trackBar3.SmallChange = 5;
+            this.trackBar3.TabIndex = 10;
+            this.trackBar3.TickFrequency = 10;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.LargeChange = 10;
+            this.trackBar4.Location = new System.Drawing.Point(635, 68);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar4.Size = new System.Drawing.Size(45, 231);
+            this.trackBar4.SmallChange = 5;
+            this.trackBar4.TabIndex = 11;
+            this.trackBar4.TickFrequency = 10;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(479, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(530, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(581, 306);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(632, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "label8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(554, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // sendTimer
+            // 
+            this.sendTimer.Enabled = true;
+            this.sendTimer.Interval = 200;
+            this.sendTimer.Tick += new System.EventHandler(this.sendTimer_Tick);
+            // 
+            // label_Acc
+            // 
+            this.label_Acc.AutoSize = true;
+            this.label_Acc.Location = new System.Drawing.Point(383, 85);
+            this.label_Acc.Name = "label_Acc";
+            this.label_Acc.Size = new System.Drawing.Size(52, 13);
+            this.label_Acc.TabIndex = 17;
+            this.label_Acc.Text = "Acc Data";
+            // 
+            // label_telemetry
+            // 
+            this.label_telemetry.AutoSize = true;
+            this.label_telemetry.Location = new System.Drawing.Point(383, 165);
+            this.label_telemetry.Name = "label_telemetry";
+            this.label_telemetry.Size = new System.Drawing.Size(53, 13);
+            this.label_telemetry.TabIndex = 18;
+            this.label_telemetry.Text = "Telemetry";
+            this.label_telemetry.Click += new System.EventHandler(this.telemetry_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 345);
+            this.Controls.Add(this.label_telemetry);
+            this.Controls.Add(this.label_Acc);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label_GPS);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Drone Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_GPS;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer refresh;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer sendTimer;
+        private System.Windows.Forms.Label label_Acc;
+        private System.Windows.Forms.Label label_telemetry;
+    }
+}
+
---
+++ DroneCs/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+
+namespace DroneCs
+{
+    public partial class Form1 : Form
+    {
+
+        UDPExample UDPex;
+        bool auto;
+
+        public Form1()
+        {
+            InitializeComponent();
+            UDPex = new UDPExample();
+            label5.Text = trackBar1.Value.ToString();
+            label6.Text = trackBar2.Value.ToString();
+            label7.Text = trackBar3.Value.ToString();
+            label8.Text = trackBar4.Value.ToString();
+            if (auto)
+                button3.Text = "Auto";
+            else
+                button3.Text = "Manual";
+        }
+        
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            backgroundWorker1.RunWorkerAsync();
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //label1.Text = textBox1.Text;
+            if (PingHost(textBox1.Text))
+                label1.Text = "Ping successful";
+            else
+                label1.Text = "Cannot reach host";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            UDPExample.Run();
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void refresh_Tick(object sender, EventArgs e)
+        {
+
+ 
+            if (!string.IsNullOrEmpty(UDPExample.m_data))
+            {
+                string[] words = UDPExample.m_data.Split(';');
+                if(words[0]=="1")
+                    label_GPS.Text ="Lattitude: " + words[1] + "\n" + "Longitude: " + words[2] + "\n" + "Height: " + words[3] + "\n";
+                    label_Acc.Text = "X: " + words[4] + "\n" + "Y: " + words[5] + "\n";
+                    float voltage = (Convert.ToSingle(words[6])/1024.0f)*5.2f*(102.0f/27.0f);
+                    label_telemetry.Text = "Voltage: " + voltage +"\n" + "per cell: "+voltage/3;
+                
+            }
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label5.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = trackBar2.Value.ToString();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            label7.Text = trackBar3.Value.ToString();
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            label8.Text = trackBar4.Value.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            auto = !auto;
+            if (auto)
+                button3.Text = "Auto";
+            else
+                button3.Text = "Manual";
+            trackBar1.Enabled = !auto;
+            trackBar2.Enabled = !auto;
+            trackBar3.Enabled = !auto;
+            trackBar4.Enabled = !auto;
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled) MessageBox.Show("Operation was canceled");
+            else if (e.Error != null) MessageBox.Show(e.Error.Message);
+            else MessageBox.Show(e.Result.ToString());
+        }
+
+        private void sendTimer_Tick(object sender, EventArgs e)
+        {
+            string binAuto;
+            if (auto)
+                binAuto = "1";
+            else
+                binAuto = "0";
+            UDPExample.m_auto = binAuto + ";" + trackBar1.Value.ToString() + ";" + trackBar2.Value.ToString() + ";" + trackBar3.Value.ToString() + ";" + trackBar4.Value.ToString() + "\n";
+            UDPExample.SendData();
+        }
+
+        private void telemetry_Click(object sender, EventArgs e)
+        {
+
+        }  
+    }
+
+
+public class UDPExample 
+    {
+        public static string m_data;
+        public static string m_auto;
+
+        public static void Run()
+        {
+            
+            //naslouchani spustime ve vlastnim vlakne
+            Thread listener = new Thread(new ThreadStart(DoListening));
+            listener.IsBackground = true;
+            listener.Start();
+            //pockame vterinu a zasleme data
+            Thread.Sleep(3000);
+            
+            //SendData();
+        }
+        
+        static void DoListening()
+        {
+
+            UdpClient client = null;
+            try
+            {
+                //zacneme poslouchat na portu 3000
+                client = new UdpClient(8888);
+                //budeme primat z jakekoli adresy a portu
+                IPEndPoint host = new IPEndPoint(IPAddress.Any, 0);
+                Console.WriteLine("{0} : UDP posluchac ceka na data..", DateTime.Now.ToString());
+                
+                //pockame na prijeti dat
+                while (true)
+                {
+                    byte[] received = client.Receive(ref host);
+                    //ziskame z bajtu retezec
+                    string receivedString = Encoding.ASCII.GetString(received);
+                    m_data = receivedString;
+
+                    Console.WriteLine("{0} : UDP posluchac prijal od {1} data : {2}", DateTime.Now.ToString(), host.ToString(), receivedString);
+                }
+            }
+            catch (SocketException ex)
+            {
+
+                Console.WriteLine("Doslo k vyjimce z duvodu : {0}", ex.SocketErrorCode);
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        public static void SendData()
+        {
+            UdpClient client = null;
+            try
+            {
+                client = new UdpClient();
+                //pripojime se na cil, kteremu chceme zaslat data
+                client.Connect("10.10.1.59", 9999);
+                //enkodujeme retezec a zasleme na cil
+                byte[] data = Encoding.ASCII.GetBytes(m_auto+" ");
+                client.Send(data, data.Length);
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Doslo k vyjimce z duvodu : {0}", ex.SocketErrorCode);
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+        
+    }
+}
